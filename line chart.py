5ef9f173ac77a01dfb65f3556528d21a7ad1563f
ishatvv <ishatv.sharma@gmail.com>
2024-06-01 04:38:03 +0530

    line chart.py

--- line chart.py
+++ line chart.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.header('line chart')
-chart_data = pd.dataframe(np.random.randn(20, 3),
+chart_data = pd.DataFrame(np.random.randn(20, 3),
                           columns=['a','b','c'])
 st.line_chart(chart_data)
                           
